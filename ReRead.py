c204f92b805d0121efdcc7b681a5e5fed735e67c
Leah Mattingly <lmattingly6@ivytech.edu>
2024-04-30 20:54:42 -0500

    Adding more Comments to meet best coding practicies

--- ReRead.py
+++ ReRead.py
@@ -1,56 +1,47 @@
+# Import necessary libraries
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 import sqlite3
-from user import user
-from book import book
-from inventory import *
 from PIL import Image, ImageTk
 
+# Global variables
 global logged_in
 logged_in = False
 global userID
 userID = None
-global cart_empty
 
-
+# Main Page Class
 class MainPage:
     def __init__(self, master, db_connection, inventory_db_connection, cart=None):
         """
-                Initialize the main page.
+        Initialize the main page.
 
-                Args:
-                    master (tk.Tk): The master Tkinter window.
-                    db_connection: SQLite database connection for user data.
-                    inventory_db_connection: SQLite database connection for inventory data.
-                """
+        Args:
+            master (tk.Tk): The master Tkinter window.
+            db_connection: SQLite database connection for user data.
+            inventory_db_connection: SQLite database connection for inventory data.
+            cart (list): Optional parameter representing the cart.
+        """
+        # Initialize attributes
         self.master = master
         self.db_connection = db_connection
-        self.inventory_db_connection = inventory_db_connection  # Store inventory_db_connection
+        self.inventory_db_connection = inventory_db_connection
+        self.cart = cart if cart is not None else []  # Initialize cart as an empty list if not provided
+
+        # Configure master window
         self.master.title("ReRead - Main Page")
-        self.master.configure(background='#F7F7F7')  # Light grey background color
+        self.master.configure(background='#F7F7F7')
         self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        if cart is None:
-            self.cart = []  # Initialize cart as an empty list if not provided
-        else:
-            self.cart = cart  # Don't recreate the cart if reopening this page
-
-        image_path = "books.jpg"  # Adjust the path accordingly
-        image = Image.open(image_path)
-
-        # Resize the image if needed
-        image = image.resize((600, 400))  # Adjust width and height as needed
-
-        # Convert the image to a format compatible with Tkinter
+        # Load and display image
+        image_path = "books.jpg"
+        image = Image.open(image_path).resize((600, 400))
         self.photo = ImageTk.PhotoImage(image)
-
-        # Create a Label widget to display the image
         image_label = tk.Label(self.master, image=self.photo, bg='#F7F7F7')
-        image_label.image = self.photo  # Keep a reference to prevent garbage collection
+        image_label.image = self.photo
         image_label.pack()
 
-        # Introduction
+        # Introduction text
         intro_text = """
         Welcome to ReRead - where the love for books meets the joy of recycling! 
         Dive into our virtual bookstore, where every page holds a story and every purchase breathes new life into pre-loved books.
@@ -58,15 +49,16 @@ class MainPage:
         Let's rediscover the magic of reading while also caring for our planet. Happy browsing!
         """
         intro_label = tk.Label(master, text=intro_text, wraplength=400, justify="center", font=("Arial", 12),
-                               bg='#F7F7F7')  # Set background color
+                               bg='#F7F7F7')
         intro_label.pack()
 
+        # Inventory button
         inventory_button = tk.Button(master, text="Inventory", command=self.open_inventory_window, font=("Arial", 12),
-                                     bg='#007BFF', fg='white')  # Set button color
+                                     bg='#007BFF', fg='white')
         inventory_button.pack(pady=5)
 
+        # Conditionally display login and register buttons
         if not logged_in:
-            # Login and Register buttons
             login_button = tk.Button(master, text="Login", command=self.open_login_window, font=("Arial", 12),
                                      bg='#28A745', fg='white')
             login_button.pack(pady=5)
@@ -74,8 +66,7 @@ class MainPage:
             register_button = tk.Button(master, text="Register", command=self.open_register_window, font=("Arial", 12),
                                         bg='#28A745', fg='white')
             register_button.pack(pady=5)
-
-        if logged_in:
+        else:
             view_cart_button = tk.Button(master, text="View Cart", command=self.open_cart_window, font=("Arial", 12),
                                          bg='#007BFF', fg='white')
             view_cart_button.pack(pady=5)
@@ -84,18 +75,21 @@ class MainPage:
                                       bg='#DC3545', fg='white')
             logout_button.pack(pady=5)
 
+    # Method to logout user
     def logout(self):
         global logged_in
         logged_in = False
         self.master.destroy()
         main_page = MainPage(tk.Toplevel(), self.db_connection, self.inventory_db_connection)
 
+    # Method to clear cart
     def clear_cart(self):
         """
-        Clears the cart after checkout
+        Clears the cart after checkout.
         """
         self.cart = []
 
+    # Method to open inventory window
     def open_inventory_window(self):
         """
         Open the inventory window.
@@ -107,6 +101,7 @@ class MainPage:
         inventory_window.protocol("WM_DELETE_WINDOW", self.on_inventory_window_close)
         InventoryPage(inventory_window, self.db_connection, self.inventory_db_connection, self.cart, self.open_cart_window)
 
+    # Method to open cart window
     def open_cart_window(self):
         """
         Open the cart window.
@@ -118,6 +113,7 @@ class MainPage:
         cart_window.configure(bg='#F7F7F7')
         CartPage(cart_window, self.cart, self.db_connection, self.inventory_db_connection, self.clear_cart)
 
+    # Method to open login window
     def open_login_window(self):
         """
         Open the login window.
@@ -129,7 +125,7 @@ class MainPage:
         login_window.configure(bg='#F7F7F7')
         LoginPage(login_window, self.db_connection, self.inventory_db_connection)
 
-
+    # Method to open register window
     def open_register_window(self):
         """
         Open the registration window.
@@ -141,7 +137,7 @@ class MainPage:
         register_window.configure(bg='#F7F7F7')
         RegistrationPage(register_window, self.db_connection)
 
-    # Show the main window when window is closed
+    # Method to handle inventory window close event
     def on_inventory_window_close(self):
         """
         Callback when the inventory window is closed.
@@ -152,6 +148,7 @@ class MainPage:
         else:
             main()
 
+    # Method to handle cart window close event
     def on_cart_window_close(self):
         """
         Callback when the cart window is closed.
@@ -162,6 +159,7 @@ class MainPage:
         else:
             main()
 
+    # Method to handle register window close event
     def on_register_window_close(self):
         """
         Callback when the register window is closed.
@@ -172,6 +170,7 @@ class MainPage:
         else:
             main()
 
+    # Method to handle login window close event
     def on_login_window_close(self):
         """
         Callback when the login window is closed.
@@ -183,6 +182,7 @@ class MainPage:
         else:
             main()
 
+    # Method to handle main window close event
     def on_closing(self):
         """
         Callback when the main window is closed.
@@ -195,10 +195,8 @@ class MainPage:
         self.master.quit()
         self.master.destroy()
 
+# Sell Page Class
 class SellPage:
-    """
-    Class representing the page for selling a book.
-    """
     def __init__(self, master, inventory_db_connection):
         """
         Initialize the sell page.
@@ -207,6 +205,7 @@ class SellPage:
             master (tk.Tk): The master Tkinter window.
             inventory_db_connection: SQLite database connection for inventory data.
         """
+        # Initialize attributes
         self.master = master
         self.inventory_db_connection = inventory_db_connection
         self.master.title("ReRead - Sell Book")
@@ -229,10 +228,12 @@ class SellPage:
         self.quantity_entry = tk.Entry(master, font=("Arial", 12))
         self.quantity_entry.pack()
 
+        # Sell button
         sell_button = tk.Button(master, text="Sell", command=self.sell_book, font=("Arial", 12),
                                 bg='#007BFF', fg='white')
         sell_button.pack(pady=10)
 
+    # Method to sell book
     def sell_book(self):
         """
         Sell a book. Insert book into inventory database
@@ -242,7 +243,6 @@ class SellPage:
         author = self.author_entry.get()
         price = self.price_entry.get()
         quantity = self.quantity_entry.get()
-
 
         # Validate input
         if not title or not author or not price or not quantity:
@@ -258,11 +258,8 @@ class SellPage:
         messagebox.showinfo("Success", "Book added to inventory successfully!")
         self.master.destroy()
 
-
+# Inventory Page Class
 class InventoryPage:
-    """
-    Class representing the inventory page.
-    """
     def __init__(self, master, db_connection, inventory_db_connection, cart, open_cart_window):
         """
         Initialize the inventory page.
@@ -271,7 +268,10 @@ class InventoryPage:
             master (tk.Tk): The master Tkinter window.
             db_connection: SQLite database connection for user data.
             inventory_db_connection: SQLite database connection for inventory data.
+            cart (list): The cart.
+            open_cart_window (function): Function to open cart window.
         """
+        # Initialize attributes
         self.master = master
         self.db_connection = db_connection
         self.inventory_db_connection = inventory_db_connection
@@ -279,6 +279,7 @@ class InventoryPage:
         self.open_cart_window2 = open_cart_window
         self.master.configure(bg='#F7F7F7')
 
+        # Inventory Treeview
         self.inventory_tree = ttk.Treeview(master)
         self.inventory_tree["columns"] = ("Title", "Author", "Price", "Quantity")
         self.inventory_tree.heading("#0", text="ID")
@@ -290,10 +291,12 @@ class InventoryPage:
         self.inventory_tree.pack(padx=10, pady=10)
         self.populate_inventory()
 
+        # Refresh button
         refresh_button = tk.Button(master, text="Refresh", command=self.refresh_inventory, font=("Arial", 12),
                                    bg='blue', fg='white')
         refresh_button.pack(pady=10)
 
+        # Conditionally display sell and cart buttons
         if logged_in:
             sell_button = tk.Button(master, text="Sell", command=self.open_sell_page, font=("Arial", 12),
                                     bg='#007BFF', fg='white')
@@ -306,10 +309,10 @@ class InventoryPage:
             view_cart_button = tk.Button(master, text="View Cart", command=self.open_cart_window2, font=("Arial", 12),
                                          bg='#007BFF', fg='white')
             view_cart_button.pack(pady=5)
-
         else:
             tk.Label(master, text="Return to Main Page to Login", font=("Arial", 12), bg='#D0E7F9').pack()
 
+    # Method to populate inventory
     def populate_inventory(self):
         """
         Populate the inventory list.
@@ -321,7 +324,11 @@ class InventoryPage:
         for book in books:
             self.inventory_tree.insert("", "end", text=book[0], values=(book[1], book[2], book[5], book[6]))
 
+    # Method to refresh inventory
     def refresh_inventory(self):
+        """
+        Refresh the inventory list.
+        """
         # Clear existing items in the inventory treeview
         for item in self.inventory_tree.get_children():
             self.inventory_tree.delete(item)
@@ -329,6 +336,7 @@ class InventoryPage:
         # Repopulate the inventory treeview with updated data
         self.populate_inventory()
 
+    # Method to open sell page
     def open_sell_page(self):
         """
         Open the sell page.
@@ -338,10 +346,11 @@ class InventoryPage:
         sell_window.configure(bg='#F7F7F7')
         SellPage(sell_window, self.inventory_db_connection)
 
-
-
-
+    # Method to add book to cart
     def add_to_cart(self):
+        """
+        Add book to cart.
+        """
         # Get the selected item from the inventory treeview
         selected_item = self.inventory_tree.selection()
         if not selected_item:
@@ -353,8 +362,8 @@ class InventoryPage:
         book_title = self.inventory_tree.item(selected_item, "values")[0]
         book_author = self.inventory_tree.item(selected_item, "values")[1]
         book_price = self.inventory_tree.item(selected_item, "values")[2]
-
         book_quantity = int(self.inventory_tree.item(selected_item, "values")[3])
+
         if book_quantity <= 0:
             messagebox.showerror("Error", "This book is out of stock.")
             return
@@ -375,9 +384,20 @@ class InventoryPage:
 
         messagebox.showinfo("Success", f"Book '{book_title}' added to cart.")
 
-
+# Cart Page Class
 class CartPage:
     def __init__(self, master, cart, db_connection, inventory_db_connection, clear_cart2):
+        """
+        Initialize the cart page.
+
+        Args:
+            master (tk.Tk): The master Tkinter window.
+            cart (list): The cart.
+            db_connection: SQLite database connection for user data.
+            inventory_db_connection: SQLite database connection for inventory data.
+            clear_cart2 (function): Function to clear the cart.
+        """
+        # Initialize attributes
         self.master = master
         self.cart = cart
         self.db_connection = db_connection
@@ -399,12 +419,19 @@ class CartPage:
                                     bg='#007BFF', fg='white')
         checkout_button.pack(pady=10)
 
-
+    # Method to display cart items
     def display_cart_items(self):
+        """
+        Display items in the cart.
+        """
         for item in self.cart:
             self.cart_tree.insert("", "end", values=(item['title'], item['author'], item['price']))
 
+    # Method to checkout
     def checkout(self):
+        """
+        Checkout the items in the cart.
+        """
         # Insert cart items into user_purchases table
         cursor = self.db_connection.cursor()
         for item in self.cart:
@@ -415,7 +442,7 @@ class CartPage:
         messagebox.showinfo("Success", "Checkout successful!")
         self.master.destroy()
 
-
+# Registration Page Class
 class RegistrationPage:
     """
     Class representing the registration page.
@@ -429,6 +456,7 @@ class RegistrationPage:
             master (tk.Tk): The master Tkinter window.
             db_connection: SQLite database connection for user data.
         """
+        # Initialize attributes
         self.master = master
         self.db_connection = db_connection
         self.master.title("ReRead - Registration")
@@ -460,7 +488,11 @@ class RegistrationPage:
                                     bg='#007BFF', fg='white')
         register_button.pack(pady=10)
 
+    # Method to register user
     def register_user(self):
+        """
+        Register a new user.
+        """
         username = self.username_entry.get()
         password = self.password_entry.get()
 
@@ -494,6 +526,7 @@ class RegistrationPage:
         self.username_entry.delete(0, tk.END)
         self.password_entry.delete(0, tk.END)
 
+# Login Page Class
 class LoginPage:
     """
     Class representing the login page.
@@ -507,6 +540,7 @@ class LoginPage:
             db_connection: SQLite database connection for user data.
             inventory_db_connection: SQLite database connection for inventory data.
         """
+        # Initialize attributes
         self.master = master
         self.db_connection = db_connection
         self.inventory_db_connection = inventory_db_connection
@@ -540,76 +574,45 @@ class LoginPage:
                                  bg='#007BFF', fg='white')
         login_button.pack(pady=10)
 
+    # Method to login user
     def login_user(self):
         """
         Log in the user.
         """
         username = self.username_entry.get()
         password = self.password_entry.get()
-        global logged_in
-        global userID
 
         if not username or not password:
-            messagebox.showerror("Error", "Please enter both username and password.")
+            tk.messagebox.showerror("Error", "Please enter both username and password.")
             return
 
-        # Check if username and password match
+        # Check if username exists
         cursor = self.db_connection.cursor()
         cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
         user = cursor.fetchone()
 
         if user:
-            userID = user[0]
-            messagebox.showinfo("Success", "Login successful!")
+            global logged_in, userID
             logged_in = True
-            self.master.withdraw()
+            userID = user[0]  # Set the user ID to the logged-in user's ID
+            tk.messagebox.showinfo("Success", "Login successful!")
             self.master.destroy()
-            main_page = MainPage(tk.Toplevel(), self.db_connection, self.inventory_db_connection)
-
         else:
-            messagebox.showerror("Error", "Invalid username or password.")
+            tk.messagebox.showerror("Error", "Invalid username or password. Please try again.")
 
-
+# Function to open the main page
 def main():
-    # Create a SQLite database connection
-    db_connection = sqlite3.connect("user_database.db")
-    cursor = db_connection.cursor()
-
-    # Create users table if not exists
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        username TEXT UNIQUE,
-                        password TEXT)''')
-
-    # Create users table if not exist
-    cursor.execute('''CREATE TABLE IF NOT EXISTS user_purchases (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        user_id INTEGER,
-                        item_no INTEGER,
-                        book_title TEXT,
-                        author TEXT,
-                        price REAL,
-                        quantity INTEGER,
-                        FOREIGN KEY (user_id) REFERENCES users(id))''')
-
-    inventory_db_connection = sqlite3.connect("inventory_database.db")
-    cursor_inventory = inventory_db_connection.cursor()
-
-    # Create inventory table if not exists
-    cursor_inventory.execute('''CREATE TABLE IF NOT EXISTS inventory (
-                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                 title TEXT,
-                                 author TEXT,
-                                 ISBN TEXT,
-                                 condition TEXT,
-                                 price REAL,
-                                 quantity INTEGER)''')
-
+    """
+    Open the main page.
+    """
     root = tk.Tk()
+    root.geometry("800x600")
+    root.resizable(False, False)
+    db_connection = sqlite3.connect("user.db")
+    inventory_db_connection = sqlite3.connect("inventory.db")
     main_page = MainPage(root, db_connection, inventory_db_connection)
     root.mainloop()
 
-
-
+# Run the application
 if __name__ == "__main__":
     main()
