4b1685a3382ed78aebe8b53144bb7d9dda022815
Mikec1986 <117950304+Mikec1986@users.noreply.github.com>
2024-05-10 15:40:34 -0500

    commenting

--- book.py
+++ book.py
@@ -1,14 +1,15 @@
 """
 Name: book.py
-Authors: Michael Coughlin,
-Date Last Updated: April 20, 2024
+Authors: Michael Coughlin, Leah Mattingly, Aubrie McIntyre, Perrin Brumfield, Gautam Mehla
+Date Last Updated: May 9th, 2024
 Description: Class file for books
 """
 
 class Book:
-    def __init__(self, title, author, ISBN, condition, price):
+    """
+    Class for Books to be sold
+    """
+    def __init__(self, title, author, price):
         self.title = title
         self.author = author
-        self.ISBN = ISBN
-        self.condition = condition
         self.price = price
